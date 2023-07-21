64c39d16a6c4906dcd6c4f944cefe008c049a03c
Farhan Naufaldy <farhan20ti@mahasiswa.pcr.ac.id>
2023-07-22 00:38:38 +0700

    register feature added

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAC_6CZcn61_i0DZ4fFKWMkOPRqItvNswQ",
+  authDomain: "sustainify-67c1e.firebaseapp.com",
+  databaseURL: "https://sustainify-67c1e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sustainify-67c1e",
+  storageBucket: "sustainify-67c1e.appspot.com",
+  messagingSenderId: "673118394696",
+  appId: "1:673118394696:web:4ce9922d360167fe8a7b7d",
+  measurementId: "G-FFMBQER2WZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const rldb = getDatabase(app);
+const frdb = getFirestore(app);
+
+export {rldb, frdb};
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -4,20 +4,85 @@
 	import welcome_fallback from '$lib/images/svelte-welcome.png';*/
 	import vector_login from '$lib/images/vector-login.png';
 	import logo from '$lib/images/logo-sustainify.png';
+	import loading from '$lib/images/loading.gif';
 	import { onMount } from 'svelte';
+	import {frdb} from "$lib/firebaseConfig.js";
+	import {doc, setDoc, getDocs, collection } from "firebase/firestore"; 
 
 	let hidden_state = 0;
 	let overflow = null;
+	let email = null;
+	let full_name = null;
+	let username = null;
+	let password = null;
+	let confirm_password = null;
+	let messageModal = 0;
+	let messageModalSuccess = 0;
+	let messagePayload = null;
+	let username_list = [];
+	let email_list = [];
 
 	function isOverflowY(element) {
 	  return element.scrollHeight != Math.max(element.offsetHeight, element.clientHeight)
 	}
+
+	// access the db collection
+	const getUserIds = async () => {
+	    const querySnapshot1 = await getDocs(collection(frdb, "users"));
+	    const querySnapshot2 = await getDocs(collection(frdb, "users"));
+	    querySnapshot1.forEach((doc) => 
+	    	username_list.push(doc.id)
+	    );
+	    querySnapshot2.forEach((doc) => 
+	    	email_list.push(doc.data().email)
+	    );
+	}
+
+	const setUserData = async (email, full_name, username, password) => {
+		await setDoc(doc(frdb, "users", username), {
+		  email: email,
+		  full_name: full_name,
+		  password: password
+		});
+	}
+
+	onMount(async() => {
+		getUserIds();
+	})
+
 </script>
 
 <svelte:head>
-	<title>Login</title>
-	<meta name="description" content="Login Page" />
+	<title>Register</title>
+	<meta name="description" content="Register Page" />
 </svelte:head>
+
+{#if messageModal == 1}
+	<div class="modal-backdrop">
+		<div class="flex flex-center-vertical flex-center-horizontal h-100">
+			<div class="card w-80 flex flex-direction-col flex-gap-semi-large flex-center-vertical flex-center-horizontal">
+				<div class="head-input-primary text-center">{messagePayload}</div>
+				<button class="btn-modal w-100" on:click={() => {
+					messageModal = 0
+				}}>Close</button>
+			</div>
+		</div>
+	</div>
+{/if}
+
+{#if messageModalSuccess == 1}
+	<div class="modal-backdrop">
+		<div class="flex flex-center-vertical flex-center-horizontal h-100">
+			<div class="card w-80 flex flex-direction-col flex-gap-regular flex-center-vertical flex-center-horizontal">
+				<div class="head-input-primary text-center">{messagePayload}</div>
+				<div class="flex flex-direction-col flex-gap-semi-large flex-center-vertical">
+					<div class="loading-text text-center">Please wait a moment</div>
+					<img src="{loading}" class="w-30">
+				</div>
+			</div>
+		</div>
+	</div>
+{/if}
 
 <section class="bg-secondary vw-100 vh-100 flex flex-direction-col page-pad">
 	<div class="vw-100 h-10 flex flex-center-vertical page-top">
@@ -28,30 +93,72 @@
 			<img src="{logo}" alt="" class="w-50">
 		</div>
 	</div>
-	<div class="bg-primary vw-100 {overflow == true ? "h-fit" : "mh-90"} template-form-bg flex flex-direction-col flex-gap-large" id="form-login">
+	<div class="bg-primary vw-100 h-fit template-form-bg flex flex-direction-col flex-gap-large" id="form-login">
 		<div class="flex flex-direction-col flex-gap-semi-large">
 			<div class="flex flex-direction-col flex-gap-regular">
 				<div class="head-input-secondary">Email</div>
-				<input type="email" name="" class="input-field w-100" placeholder="input email..">
+				<input type="email" name="" class="input-field w-100" placeholder="input email.." bind:value={email}>
 			</div>
 			<div class="flex flex-direction-col flex-gap-regular">
 				<div class="head-input-secondary">Full Name</div>
-				<input type="text" name="" class="input-field w-100" placeholder="input full name..">
+				<input type="text" name="" class="input-field w-100" placeholder="input full name.." bind:value={full_name}>
 			</div>
 			<div class="flex flex-direction-col flex-gap-regular">
 				<div class="head-input-secondary">Username</div>
-				<input type="text" name="" class="input-field w-100" placeholder="input username..">
+				<input type="text" name="" class="input-field w-100" placeholder="input username.." bind:value={username}>
 			</div>
 			<div class="flex flex-direction-col flex-gap-regular">
 				<div class="head-input-secondary">Password</div>
-				<input type="password" name="" class="input-field w-100" placeholder="input password..">
+				<input type="password" name="" class="input-field w-100" placeholder="input password.." bind:value={password}>
 			</div>
 			<div class="flex flex-direction-col flex-gap-regular">
 				<div class="head-input-secondary">Confirm Password</div>
-				<input type="password" name="" class="input-field w-100" placeholder="input confirm password..">
+				<input type="password" name="" class="input-field w-100" placeholder="input confirm password.." bind:value={confirm_password}>
 			</div>
 			<div class="flex flex-direction-col flex-gap-semi-large padding-btn-login">
-				<button class="btn-secondary w-100">Sign Up</button>
+				<button class="btn-secondary w-100" on:click={() => {
+					if (email == "" || email == null) {
+						messageModal = 1;
+						messagePayload = "Please fill your email";
+					} else if(full_name == "" || full_name == null) {
+						messageModal = 1;
+						messagePayload = "Please fill your full name";
+					} else if(username == "" || username == null) {
+						messageModal = 1;
+						messagePayload = "Please fill your username";
+					} else if(password == "" || password == null) {
+						messageModal = 1;
+						messagePayload = "Please fill your password";
+					} else if(confirm_password == "" || confirm_password == null) {
+						messageModal = 1;
+						messagePayload = "Please confirm your password";
+					} else {
+						if (password != confirm_password) {
+							messageModal = 1;
+							messagePayload = "Your password doesn't match";
+						} else {
+							for (var i = 0; i < username_list.length; i++) {
+								if (username_list[i] == username) {
+									messageModal = 1;
+									messagePayload = "The username has been taken";
+									break;
+								} else if(i == username_list.length - 1) {
+									for (var i = 0; i < email_list.length; i++) {
+										if (email_list[i] == email) {
+											messageModal = 1;
+											messagePayload = "The email has been registered";
+											break;
+										} else if(i == email_list.length - 1) {
+											setUserData(email, full_name, username, password);
+											messageModalSuccess = 1;
+											messagePayload = "Registration successful";
+										}
+									}
+								}
+							}
+						}
+					}
+				}}>Sign Up</button>
 			</div>
 		</div>
 	</div>
