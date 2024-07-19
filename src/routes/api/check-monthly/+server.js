91ce2677a0102296095762da35d8ed9b1eba7f78
Farhan Naufaldy <farhan20ti@mahasiswa.pcr.ac.id>
2024-07-19 09:52:57 +0700

    monthly resetter

--- src/routes/api/check-monthly/+server.js
+++ src/routes/api/check-monthly/+server.js
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 import {frdb, strg} from "$lib/firebaseConfig.js";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import {doc, setDoc, getDocs, collection } from "firebase/firestore"; 
+import {doc, setDoc, getDocs, collection, getDoc, updateDoc } from "firebase/firestore"; 
 import { config as dotenvConfig } from 'dotenv';
 import webPush from "web-push"
 let month_year_list = [];
@@ -39,79 +39,85 @@ export async function GET({ request }) {
           let username = documentsUser[i].id
           const queryCarbon = await getDocs(collection(frdb, "users", username, "carbon-record"));
           const queryMission = await getDocs(collection(frdb, "users", username, "missions"));
+          const missionRef = await doc(frdb, 'users', username, "missions", "mission_2");
+          const userRef = await doc(frdb, 'users', username);
+          const getDataMission = await getDoc(missionRef);
           const documentsCarbon = queryCarbon.docs;
           const documentsLengthCarbon = documentsCarbon.length;
+          const documentsMission = queryMission.docs;
+          const documentsLengthMission = documentsMission.length;
           let user_datas = documentsUser[i].data();
-          if (user_datas.endpoint != undefined) {
-            console.log(user_datas.endpoint)
-            const vapidKeys = {
-              publicKey: import.meta.env.VITE_VAPID_PUBLIC_KEY,
-              privateKey: "hUf1juDtj3g0w7GZMYbfPZhZRTX7BgN3J8mmQhYeSJo"
-            };
-            console.log(vapidKeys)
-            webPush.setVapidDetails(
-              'mailto:farhan.naufaldy2811@gmail.com',
-              vapidKeys.publicKey,
-              vapidKeys.privateKey
-            );
-            const payload = JSON.stringify({ title: 'test', body: `${currentMonthYear}` });
-            webPush.sendNotification({endpoint: user_datas.endpoint, keys : {auth: user_datas.keys.auth, p256dh: user_datas.keys.p256dh}}, payload).then(response => {
-              console.log('Push notification sent:', response);   
-            }).catch(error => {
-              console.error('Error sending push notification:', error);
-            })
+          console.log(username)
+          if (documentsLengthCarbon == 0) {
+           await updateDoc(missionRef, {
+              goal: 0
+            });
+          } else if (documentsLengthCarbon == 1){
+            console.log("masuk")
+            for (var x = 0; x < documentsLengthCarbon; x++) {
+              let carbon_data = documentsCarbon[x].data();
+              await updateDoc(missionRef, {
+                goal: carbon_data.vehicle_fuel
+              });
+            }
+          } else {
+            const getDataCarbon = await getDoc(collection(frdb, "users", username, "carbon-record", monthNames[month-1]+"-"+year));
+            if (getDataCarbon.data().vehicle_fuel >= getDataMission.data().goal) {
+              await updateDoc(missionRef, {
+                goal: getDataCarbon.data().vehicle_fuel
+              });
+            } else {
+              await updateDoc(userRef, {
+                  points: increment(getDataMission.data().points)
+              });
+              await updateDoc(missionRef, {
+                  state: "finished",
+                  goal: getDataCarbon.data().vehicle_fuel
+              });
+              const notifCollection = collection(frdb, 'notifications');
+                const notifId = doc(notifCollection).id;
+                if (user_datas.endpoint != undefined) {
+                  const vapidKeys = {
+                    publicKey: import.meta.env.VITE_VAPID_PUBLIC_KEY,
+                    privateKey: "hUf1juDtj3g0w7GZMYbfPZhZRTX7BgN3J8mmQhYeSJo"
+                  };
+                  console.log(vapidKeys)
+                  webPush.setVapidDetails(
+                    'mailto:farhan.naufaldy2811@gmail.com',
+                    vapidKeys.publicKey,
+                    vapidKeys.privateKey
+                  );
+                  const payload = JSON.stringify({ title: 'Congratulations!', body: 'You just completed one of your missions!' });
+                  webPush.sendNotification({endpoint: user_datas.endpoint, keys : {auth: user_datas.keys.auth, p256dh: user_datas.keys.p256dh}}, payload).then(response => {
+                    console.log('Push notification sent:', response);   
+                  }).catch(error => {
+                    console.error('Error sending push notification:', error);
+                  })
+                }
+                await setDoc(doc(notifCollection, notifId), {
+                  user_id: username,
+                  message: `You just completed one of your missions!`,
+                  page_url: '/home',
+                  timestamp : `${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`
+                });
+                await updateDoc(userRef, {
+                  notification_count: increment(1)
+               });
+            }
           }
-          // const vapidKeys = {
-          //   publicKey: import.meta.env.VITE_VAPID_PUBLIC_KEY,
-          //   privateKey: "hUf1juDtj3g0w7GZMYbfPZhZRTX7BgN3J8mmQhYeSJo"
-          // };
-          // console.log(subscription)
-          // console.log(vapidKeys)
-          // webPush.setVapidDetails(
-          //   'mailto:farhan.naufaldy2811@gmail.com',
-          //   vapidKeys.publicKey,
-          //   vapidKeys.privateKey
-          // );
-          // const payload = JSON.stringify({ title: 'test', body: `${currentMonthYear}` });
-          // webPush.sendNotification(subscription, payload).then(response => {
-          //   console.log('Push notification sent:', response);   
-          // }).catch(error => {
-          //   console.error('Error sending push notification:', error);
-          // });
-          // return json(subscription);
-          // if (DocumentsLengthCarbon > 0) {
-
-          // }
+          console.log(documentsLengthMission)
+          for (var j = 0; j < documentsLengthMission; j++) {
+            let missionUpdateRef = await doc(frdb, 'users', username, "missions", documentsMission[j].id);
+            await updateDoc(missionUpdateRef, {
+                  state: "unfinished"
+              });
+            console.log("updated mission" + j)
+          }
         }
      }
-     return json({ success: false, error: 'Failed to save subscription' });
-
-
-      // const queryCarbon = await getDocs(collection(frdb, "users", localStorage.getItem("username"), "carbon-record"));
-      // const documentsCarbon = queryCarbon.docs;
-      // const documentsLengthCarbon = documentsCarbon.length;
-
-    // const subscription = await request.json();
-    // const vapidKeys = {
-    //   publicKey: import.meta.env.VITE_VAPID_PUBLIC_KEY,
-    //   privateKey: "hUf1juDtj3g0w7GZMYbfPZhZRTX7BgN3J8mmQhYeSJo"
-    // };
-    // console.log(vapidKeys)
-    // webPush.setVapidDetails(
-    //   'mailto:farhan.naufaldy2811@gmail.com',
-    //   vapidKeys.publicKey,
-    //   vapidKeys.privateKey
-    // );
-    // const payload = JSON.stringify({ title: 'test', body: `${currentMonthYear}` });
-    // webPush.sendNotification(subscription, payload).then(response => {
-    //   console.log('Push notification sent:', response);   
-    // }).catch(error => {
-    //   console.error('Error sending push notification:', error);
-    // });
-    // return json(subscription);
+     return json({ success: true, error: 'succeed' });
   } catch (error) {
-    return json({ success: false, error: 'Failed to save subscription' });
-    // console.error('Error saving subscription:', error);
-    // return json({ success: false, error: 'Failed to save subscription' });
+    console.log(error)
+    return json({ success: false, error: 'failed' });
   }
 }
